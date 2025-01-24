6cabedfb4f31dfc399d707270c2d7e3a08001d1d
MHchihuahua <minhsienhung@gmail.com>
2025-01-24 17:32:14 +0800

    feat: update Projects page layout and content

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,16 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: '【計畫】設計情境創新工具研究',
+    description: `協助成大「設計情境創新工具研究」計畫，利用 AI 技術輔助設計思考流程。開發基於 Python Flask 和 ChatGPT API 的即時網頁應用，透過 SSE 技術實現資料即時更新，並模擬 ChatGPT 打字效果，提升互動性與使用者體驗。`,
+    imgSrc: '/static/images/projects/designThinking.png',
+    href: '',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: '【論文】車流量計數互動元件',
+    description: `使用 Fabric.js 協助 Lab 投稿論文的互動元件製作，論文主旨為利用 YOLO 與 DeepSORT 進行車流辨識與計數，透過互動元件可以了解這兩項技術是如何應用在車流上`,
+    imgSrc: '/static/images/projects/AI-X.png',
+    href: 'https://smalloshin.github.io/AI-X/',
   },
 ]
 
