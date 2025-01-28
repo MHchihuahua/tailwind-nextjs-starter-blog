67a785bbe3e4a9be4e8089843d338064a29e766d
MHchihuahua <minhsienhung@gmail.com>
2025-01-28 16:53:46 +0800

    chore: update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,8 @@ export default function Footer() {
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
         </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+        <div className="mb-8 text-xs text-gray-400 dark:text-gray-600">
+          Modified from{' '}
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
             Tailwind Nextjs Theme
           </Link>
